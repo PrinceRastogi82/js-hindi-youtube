f6bacf29477d45e7ebffa50cc6900fcccf93bc79
PrinceRastogi82 <p8433015897@gmail.com>
2026-01-27 11:17:06 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let accountEmail = "abcs2gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+
+/*
+let is used instead of let because of function scope and block scope
+* to avoid redeclaration
+*/
+
+console.table([accountEmail, accountId, accountPassword, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all JS code as newer version
+
+
+let name = "Prince"
+let age = 20
+let loggedIn = false
+let state;
+
+// number => 2^53
+// bigint
+// string =""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) // output -> undefined
+console.log(typeof null) // output -> object
