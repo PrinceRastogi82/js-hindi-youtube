2413128643b82b95cdbc119e182506e36dbfde1c
PrinceRastogi82 <p8433015897@gmail.com>
2026-01-31 11:14:22 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+//let score = "120"
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+//let InNumber = Number(score)
+//console.log(typeof score)
+//console.log(InNumber)
+
+
+// "120" -> 33 (number will get easily converted to a num)
+// "33abc" -> NaN (not a number)
+// true -> 1; false -> 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 -> true; 0 -> false
+// "" -> false
+// "Prince" -> true
+
