eb548faebc2d52956cbf5076ab8fdfaed8fbe0de
PrinceRastogi82 <p8433015897@gmail.com>
2026-02-04 14:11:13 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+//console.log(null > 0);       the reason is that an equality check == and comparisons >,<,>=,<+ work differently. 
+//console.log(null == 0);      Compariosns convert null to a number, treating it as 0.
+//console.log(null >= 0);      that's why (3) null >= 0 is true and (1) null > 0 is false
+
+// ===
+
+//console.log("2" === 2)
